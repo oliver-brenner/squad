6acc063fff1f47ad11d914fbd64c81b3a2acb2a4
Oliver Brenner <oliver.brenner@beatport.com>
2026-05-16 16:17:52 +0100

    remove new session button from homepage

--- src/routes/app-layout.tsx
+++ src/routes/app-layout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import { QueryProvider } from "@/components/providers/query-provider";
 import { UserFieldOptionsProvider } from "@/components/providers/user-field-options-provider";
 import { BottomNav } from "@/components/nav/bottom-nav";
-import { NewSessionPin } from "@/components/nav/new-session-pin";
 
 // Shell shared by every authenticated route. Mounts inside AuthGuard (auth
 // already enforced) and PowerSyncProvider (local DB initialised + bootstrap
@@ -12,11 +11,10 @@ export function AppLayout() {
     <QueryProvider>
       <UserFieldOptionsProvider>
         <div className="flex min-h-dvh flex-col">
-          <main className="flex-1 mx-auto w-full max-w-2xl px-4 pb-32">
+          <main className="flex-1 mx-auto w-full max-w-2xl px-4 pb-24">
             <Outlet />
           </main>
           <div className="fixed inset-x-0 bottom-0 z-40">
-            <NewSessionPin />
             <BottomNav />
           </div>
         </div>
